2d731dbbddc38db55bc2cda388b70fab088553d1
SAI SRIRAM KAMINENI <kaminenisaisriram@gmail.com>
2024-09-01 19:55:53 +0530

    index.js

--- BLOGIFY-backend-main/index.js
+++ BLOGIFY-backend-main/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors({
-  origin: 'https://blogify-mern-bwdy.vercel.app',
+  origin: 'https://blogify-mern-alpha.vercel.app/',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true // This allows cookies to be sent with requests
